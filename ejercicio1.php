fcd287166f2179360732d2f37638be6fd95d2bff
mitscker <antonio.mits357@gmail.com>
2022-07-13 18:14:06 -0500

    exercise 1 - declarate two variables - print variables

---
+++ ejercicio1.php
@@ -0,0 +1,10 @@
+<h2>Ejercicio 1</h2>
+<?php 
+
+    // printing two variables
+    $uno = "variable cadena"; // string
+    $dos = 45; // int
+
+    echo "UNO: ".$uno."<br/>";
+    echo "DOS: ".$dos."<hr/>";
+?>
