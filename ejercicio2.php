7a71550f88f29130bed7f84f8246d2ee70baa277
mitscker <antonio.mits357@gmail.com>
2022-07-13 19:30:28 -0500

    cicle for
    - use cicle for

---
+++ ejercicio2.php
@@ -0,0 +1,9 @@
+<h2>Ejercicio 2</h2>
+<?php 
+
+    // print on display square of natural numbers
+    for($i = 1;$i<=30;$i++) {
+        echo "El cuadrado de ".$i." es:".($i*$i)."<br/>";
+    }
+
+?>
