49315c1be3f6419211cd91b54ccd0090a9557f06
mitscker <antonio.mits357@gmail.com>
2022-07-13 22:58:28 -0500

    exercise 7
    - using cicle foreach with array

---
+++ ejercicio7.php
@@ -0,0 +1,25 @@
+<h2>Ejercicio 7</h2>
+<?php 
+
+    // create months array and display each value with a cicle foreach
+    $meses = array(
+        "Enero",
+        "Febrero",
+        "Marzo",
+        "Abril",
+        "Mayo",
+        "Junio",
+        "Julio",
+        "Agosto",
+        "Septiembre",
+        "Octubre",
+        "Noviembre",
+        "Diciembre",
+    );
+
+    foreach($meses as $month) {
+        echo "<p>".$month."</p>";
+    }
+
+    
+?>
